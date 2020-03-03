423f4426c32b07be21571275615f7d3776e7d08d
Krzysztof Kukla <krzys.kukla@gmail.com>
2020-03-03 18:40:28 +0100

    224_assigment State Machine Transitions, closes #11

--- src/main/java/pl/kukla/krzys/msscbeerorderservice/config/BeerOrderStateMachineConfig.java
+++ src/main/java/pl/kukla/krzys/msscbeerorderservice/config/BeerOrderStateMachineConfig.java
@@ -1,11 +1,10 @@
 package pl.kukla.krzys.msscbeerorderservice.config;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.statemachine.config.EnableStateMachineFactory;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
 import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
 import pl.kukla.krzys.msscbeerorderservice.domain.BeerOrderEventEnum;
 import pl.kukla.krzys.msscbeerorderservice.domain.BeerOrderStatusEnum;
 
@@ -15,8 +14,6 @@ import java.util.EnumSet;
  * @author Krzysztof Kukla
  */
 @Configuration
-@Slf4j
-@RequiredArgsConstructor
 @EnableStateMachineFactory // it scans Spring Components to generate ( create ) State Machine
 public class BeerOrderStateMachineConfig extends StateMachineConfigurerAdapter<BeerOrderStatusEnum, BeerOrderEventEnum> {
 
@@ -33,6 +30,14 @@ public class BeerOrderStateMachineConfig extends StateMachineConfigurerAdapter<B
             .end(BeerOrderStatusEnum.DELIVERY_EXCEPTION)
             .end(BeerOrderStatusEnum.VALIDATION_EXCEPTION)
             .end(BeerOrderStatusEnum.ALLOCATION_EXCEPTION);
+    }
+
+    @Override
+    public void configure(StateMachineTransitionConfigurer<BeerOrderStatusEnum, BeerOrderEventEnum> transitions) throws Exception {
+        transitions
+            .withExternal().source(BeerOrderStatusEnum.NEW).target(BeerOrderStatusEnum.NEW).event(BeerOrderEventEnum.VALIDATE_ORDER)
+            .and().withExternal().source(BeerOrderStatusEnum.NEW).target(BeerOrderStatusEnum.VALIDATED).event(BeerOrderEventEnum.VALIDATION_PASSED)
+            .and().withExternal().source(BeerOrderStatusEnum.NEW).target(BeerOrderStatusEnum.VALIDATION_EXCEPTION).event(BeerOrderEventEnum.VALIDATION_FAILED);
     }
 
 }
